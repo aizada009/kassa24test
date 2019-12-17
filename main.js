91789247cd77e30e4a33134acb4a44315e12cc32
aizada <k.aizada008@gmail.com>
2019-12-17 20:16:01 +0600

    sorted by data and delete button

--- main.js
+++ main.js
@@ -1,13 +1,28 @@
 const vm = new Vue({
   el: '#app',
   data: {
-    results: []
+    results: [],
+    counter: 48
   },
   mounted() {
-    axios.get("https://5dde338afca1110014f16122.mockapi.io/apiforbekzhan/articles")
+    axios.get("https://5dde338afca1110014f16122.mockapi.io/apiforbekzhan/articles?sortBy=createdAt&order=desc")
     .then(response => (
                 this.results = response.data 
                 ))
     .catch(error =>( console.log(error)));
-  }
+    
+
+
+  },
+ methods : {
+  deleteData(n) { 
+        console.log(n);
+        axios.delete("https://5dde338afca1110014f16122.mockapi.io/apiforbekzhan/articles/"+this.results.id)
+        .then(response => {
+        this.results.splice(n, 1)
+        console.log(this.results); 
+
+      });
+    }
+ }
 });
